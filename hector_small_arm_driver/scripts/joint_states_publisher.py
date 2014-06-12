1a007231eab6ea53d257ecc793cbc6757ddcb3bb
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2014-06-12 17:53:26 +0200

    hector_small_arm_driver: Initial commit based on catkinized previous electric version

---
+++ hector_small_arm_driver/scripts/joint_states_publisher.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2010, Arizona Robotics Research Group, University of Arizona
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#     * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.  * Redistributions
+#     in binary form must reproduce the above copyright notice, this list of
+#     conditions and the following disclaimer in the documentation and/or
+#     other materials provided with the distribution. # Neither the name of
+#     the Willow Garage, Inc. nor the names of its contributors may be used to
+#     endorse or promote products derived from this software without specific
+#     prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Modified for Team Hector of TU Darmstadt by Laura Strickland
+
+import roslib; roslib.load_manifest('hector_arm_driver') 
+import rospy
+
+from sensor_msgs.msg import JointState as JointStatePR2
+from dynamixel_msgs.msg import JointState as JointStateAX12
+
+class JointStateMessage():
+    def __init__(self, name, position, velocity, effort):
+        self.name = name
+        self.position = position
+        self.velocity = velocity
+        self.effort = effort
+
+class JointStatesPublisher():
+    def __init__(self):
+        rospy.init_node('thumper_arm_states_publisher', anonymous=True)
+                          
+        self.controllers = ('joint0_controller',
+                            'joint1_controller',
+                            'joint2_controller',
+                            'joint3_controller',
+                            'joint4_controller',
+                            'ls_roll_controller',
+                            'ls_pitch_controller')
+        self.joint_states={}                    
+                            
+        # Start controller state subscribers
+        [rospy.Subscriber(c + '/state', JointStateAX12, self.controller_state_handler) for c in self.controllers]
+        
+        # Start publisher
+        self.joint_states_pub = rospy.Publisher('joint_states', JointStatePR2)
+        
+        publish_rate = rospy.get_param('~rate', 50)
+        r = rospy.Rate(publish_rate)
+        
+        while not rospy.is_shutdown():
+            self.publish_joint_states()
+            r.sleep()
+            
+    def controller_state_handler(self, msg):
+ #       jcs = JointStateMessage(msg.name, (msg.name == ('joint4' or 'joint3' or 'joint2') ? -msg.current_pos : msg.current_pos), msg.velocity, msg.load) #Is the syntax here right?
+
+        self.joint_states[msg.name] = JointStateMessage(msg.name, msg.current_pos, msg.velocity, msg.load)
+        
+    def publish_joint_states(self):
+        # Construct message & publish joint states
+        msg = JointStatePR2()
+        msg.name = []
+        msg.position = []
+        msg.velocity = []
+        msg.effort = []
+        
+        for joint in self.joint_states.values():
+          msg.name.append(joint.name)
+          msg.position.append(joint.position)
+          msg.velocity.append(joint.velocity)
+          msg.effort.append(joint.effort)
+            
+          msg.header.stamp = rospy.Time.now()
+          self.joint_states_pub.publish(msg)
+
+if __name__ == '__main__':
+    try:
+        s = JointStatesPublisher()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
+
---
+++ hector_small_arm_driver/src/joint_state_publisher.cpp
@@ -0,0 +1,75 @@
+#include "ros/ros.h"
+
+#include <sensor_msgs/JointState.h>
+#include <dynamixel_msgs/JointState.h>
+
+class HectorJointStatePublisher{
+public:
+  HectorJointStatePublisher()
+  {
+    joint_name_vector.push_back("joint0_controller");
+    joint_name_vector.push_back("joint1_controller");
+    joint_name_vector.push_back("joint2_controller");
+    joint_name_vector.push_back("joint3_controller");
+    joint_name_vector.push_back("joint4_controller");
+    joint_name_vector.push_back("ls_roll_controller");
+    joint_name_vector.push_back("ls_pitch_controller");
+
+    out_joint_state_.name.resize(joint_name_vector.size());
+    out_joint_state_.position.resize(joint_name_vector.size());
+
+    ros::NodeHandle nh;
+
+    joint_state_pub_ = nh.advertise<sensor_msgs::JointState>("/joint_states",5);
+
+    for (size_t i = 0; i < joint_name_vector.size(); ++i){
+      //dyn_joint_state_subs_[i] = nh.subscribe("/" + joint_name_vector[i] + "/state", 1, &HectorJointStatePublisher::dynamixelJointStateCallback, this);
+      //ros::Subscriber sub = nh.subscribe("/" + joint_name_vector[i] + "/state", 1, &HectorJointStatePublisher::dynamixelJointStateCallback, this);
+      dyn_joint_state_subs_.push_back(nh.subscribe("/" + joint_name_vector[i] + "/state", 1, &HectorJointStatePublisher::dynamixelJointStateCallback, this));
+      out_joint_state_.name[i] = joint_name_vector[i];
+      out_joint_state_.position[i] = 0.0;
+      map_name_to_index_[joint_name_vector[i]] = i;
+    }
+
+    ros::Duration joint_state_publish_period(1/40.0);
+
+    joint_state_publish_timer_ = nh.createTimer(ros::Duration(joint_state_publish_period), &HectorJointStatePublisher::timerPublishJointStates, this, false );
+
+  }
+
+  ~HectorJointStatePublisher()
+  {
+
+  }
+
+  void dynamixelJointStateCallback(const dynamixel_msgs::JointState dyn_joint_state)
+  {
+    out_joint_state_.position[ map_name_to_index_[dyn_joint_state.name] ] = dyn_joint_state.current_pos;
+  }
+
+  void timerPublishJointStates(const ros::TimerEvent& e)
+  {
+    out_joint_state_.header.stamp = ros::Time::now();
+    joint_state_pub_.publish(out_joint_state_);
+  }
+
+protected:
+  std::vector<std::string> joint_name_vector;
+  std::vector<ros::Subscriber> dyn_joint_state_subs_;
+  ros::Publisher joint_state_pub_;
+  std::map<std::string, int> map_name_to_index_;
+
+  ros::Timer joint_state_publish_timer_;
+  sensor_msgs::JointState out_joint_state_;
+};
+
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "hector_arm_joint_state_publisher");
+
+  HectorJointStatePublisher jp;
+
+  ros::spin();
+
+  return 0;
+}
